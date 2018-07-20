472bce5a2855b1b001bcbdc183142d64dc981ea2
lbj <532413861@qq.com>
2018-07-20 09:41:47 +0800

    tijiaobule

---
+++ dist/js/controller/test.js
@@ -0,0 +1,36 @@
+var myApp=angular.module('app',[]);
+myApp.controller('test' ,function($scope,$http){
+	$scope.userInfos = [{
+		'userName': '周毅',
+		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
+		'description': '已完成99单，好评率99%',
+	},{
+		'userName': '周毅',
+		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
+		'description': '已完成99单，好评率99%',
+	},{
+		'userName': '周毅',
+		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
+		'description': '已完成99单，好评率99%',
+	},{
+		'userName': '周毅',
+		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
+		'description': '已完成99单，好评率99%',
+	},{
+		'userName': '周毅',
+		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
+		'description': '已完成99单，好评率99%',
+	},{
+		'userName': '周毅',
+		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
+		'description': '已完成99单，好评率99%',
+	},{
+		'userName': '周毅',
+		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
+		'description': '已完成99单，好评率99%',
+	},{
+		'userName': '周毅',
+		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
+		'description': '已完成99单，好评率99%',
+	},];
+});

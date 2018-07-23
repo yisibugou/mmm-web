419e88c5e08898f1eae2e7352e8881183c681f1f
周毅 <nihaozhouyi@163.com>
2018-07-24 01:33:04 +0800

    首页修改，登录，注册

--- dist/js/controller/test.js
+++ dist/js/controller/test.js
@@ -1,5 +1,5 @@
-var myApp=angular.module('app',[]);
-myApp.controller('test' ,function($scope,$http){
+var myApp=angular.module('app', []);
+myApp.controller('test', function($scope, $http){
 	$scope.userInfos = [{
 		'userName': '周毅',
 		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
@@ -33,4 +33,44 @@ myApp.controller('test' ,function($scope,$http){
 		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
 		'description': '已完成99单，好评率99%',
 	},];
+});
+
+myApp.controller('tasks', function($scope, $http){
+	$scope.tasks = [{
+		'title' : '图书馆管理系统',
+		'reward' : '100',
+		'period' : '2',
+		'deadline': '2018-07-24 18:18:18',
+		'demandUser' : '卢宝嘉'
+	},{
+		'title' : '图书馆管理系统',
+		'reward' : '100',
+		'period' : '2',
+		'deadline': '2018-07-24 18:18:18',
+		'demandUser' : '卢宝嘉'
+	},{
+		'title' : '图书馆管理系统',
+		'reward' : '100',
+		'period' : '2',
+		'deadline': '2018-07-24 18:18:18',
+		'demandUser' : '卢宝嘉'
+	},{
+		'title' : '图书馆管理系统',
+		'reward' : '100',
+		'period' : '2',
+		'deadline': '2018-07-24 18:18:18',
+		'demandUser' : '卢宝嘉'
+	},{
+		'title' : '图书馆管理系统',
+		'reward' : '100',
+		'period' : '2',
+		'deadline': '2018-07-24 18:18:18',
+		'demandUser' : '卢宝嘉'
+	},{
+		'title' : '图书馆管理系统',
+		'reward' : '100',
+		'period' : '2',
+		'deadline': '2018-07-24 18:18:18',
+		'demandUser' : '卢宝嘉'
+	}];
 });

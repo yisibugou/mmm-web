d9a6f22de0a46709d21788e8e40959e395ad3502
周毅 <nihaozhouyi@163.com>
2018-07-29 18:03:22 +0800

    首页优化，登录

--- dist/js/controller/home.js
+++ dist/js/controller/home.js
@@ -1,5 +1,11 @@
 var myApp=angular.module('app', []);
 myApp.controller('test', function($scope, $http){
+	if(localStorage.user != null) {
+		$scope.user = JSON.parse(localStorage.user);
+		$scope.isLogin = true;
+	} else {
+		$scope.isLogin = false;
+	}
 	$scope.userInfos = [{
 		'userName': '周毅',
 		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
@@ -32,10 +38,7 @@ myApp.controller('test', function($scope, $http){
 		'userName': '周毅',
 		'introduction':'阿里巴巴集团主要创始人，现担任阿里巴巴集团董事局主席、日本软银董事.......',
 		'description': '已完成99单，好评率99%',
-	},];
-});
-
-myApp.controller('tasks', function($scope, $http){
+	}];
 	$scope.tasks = [{
 		'title' : '图书馆管理系统',
 		'reward' : '100',
@@ -73,4 +76,40 @@ myApp.controller('tasks', function($scope, $http){
 		'deadline': '2018-07-24 18:18:18',
 		'demandUser' : '卢宝嘉'
 	}];
+	$scope.login = function() {
+		$http({
+			method: 'POST',
+			data: {
+				account: $scope.username,
+				password: $scope.password
+			},
+			url: 'http://localhost:8080/user/login'
+		}).then(function successCallback(response) {
+				var userStr = JSON.stringify(response.data.user);
+				localStorage.user = userStr;
+				window.location.href="http://localhost/mmm-web/html/home.html";
+			}, function errorCallback(response) {
+				alert("请求失败");
+		});
+	};
+	$scope.logout = function() {
+		localStorage.clear(); 
+		window.location.href="http://localhost/mmm-web/html/home.html";
+	};
 });
+
+
+myApp.directive("loginTmpl", function() {
+    return {
+        templateUrl : "http://localhost/mmm-web/tmpl/login.html?v=1",
+		restrict : "E"
+    };
+});
+
+myApp.directive("registerTmpl", function() {
+    return {
+        templateUrl : "http://localhost/mmm-web/tmpl/register.html",
+		restrict : "E"
+    };
+});
+
